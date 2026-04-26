c8be896d3d0fc772d24962bd67cfb32a3cc9fd1d
João Vitor Pinto <155552335+Joaovit77@users.noreply.github.com>
2026-04-26 01:06:08 -0300

    corrigindo script frontend

--- api/agendar.js
+++ api/agendar.js
@@ -7,28 +7,47 @@ process.env.SUPABASE_URL,
 process.env.SUPABASE_KEY
 );
 
-module.exports = async (req, res) => {
 
-try {
 
-const body = typeof req.body === "string"
-? JSON.parse(req.body)
-: req.body;
+module.exports = async (req,res)=>{
 
-const { nome, telefone, data, hora, servico } = body;
+    
+
+if(req.method !== 'POST'){
+return res
+.status(405)
+.send('Método inválido');
+}
+
+
+const {
+nome,
+telefone,
+data,
+hora,
+servico
+} = req.body;
+
+
 
 // procura conflito
 const { data: conflito } = await supabase
 .from('agendamentos')
 .select('*')
-.eq('data', data)
-.eq('hora', hora);
+.eq('data',data)
+.eq('hora',hora);
 
-if (conflito.length > 0) {
+
+
+if(conflito.length > 0){
+
 return res.status(400).json({
-erro: 'Horário ocupado'
+erro:'Horário ocupado'
 });
+
 }
+
+
 
 // insere
 await supabase
@@ -43,18 +62,10 @@ servico
 }
 ]);
 
-return res.json({
-ok: true,
-mensagem: 'Agendamento criado'
+
+res.json({
+ok:true,
+mensagem:'Agendamento criado'
 });
-
-} catch (error) {
-
-return res.status(500).json({
-erro: 'Erro no servidor',
-detalhe: error.message
-});
-
-}
 
 };
