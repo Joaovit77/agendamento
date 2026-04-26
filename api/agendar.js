12caa9c67f071f70c186d2f28dc0f890e8a5da0e
João Vitor Pinto <155552335+Joaovit77@users.noreply.github.com>
2026-04-26 13:36:44 -0300

    corrigindo erro 500 agendamento

--- api/agendar.js
+++ api/agendar.js
@@ -9,16 +9,30 @@ module.exports = async (req,res)=>{
 
 try{
 
-const body = typeof req.body === "string"
-? JSON.parse(req.body)
-: req.body;
+console.log("FUNÇÃO INICIOU");
 
-const { nome, telefone, data, hora, servico } = body;
+console.log("req.body:", req.body);
 
+const body = req.body || {};
 
-// SOMENTE INSERT
-const { error } = await supabase
-.from('agendamento')
+const {
+nome,
+telefone,
+data,
+hora,
+servico
+} = body;
+
+if(!nome || !data || !hora){
+
+return res.status(400).json({
+erro:"Dados não chegaram na API"
+});
+
+}
+
+const { data: inserted, error } = await supabase
+.from('agendamentos')
 .insert([
 {
 nome,
@@ -27,9 +41,12 @@ data,
 hora,
 servico
 }
-]);
+])
+.select();
 
 if(error){
+
+console.log("ERRO SUPABASE:", error);
 
 return res.status(500).json({
 erro:error.message
@@ -37,15 +54,20 @@ erro:error.message
 
 }
 
+console.log("INSERIDO:", inserted);
+
 return res.json({
 ok:true,
-mensagem:"Salvou"
+mensagem:"Salvou",
+dados: inserted
 });
 
 }catch(err){
 
+console.log("ERRO GERAL:", err);
+
 return res.status(500).json({
-erro:err.message
+erro: err.message
 });
 
 }
